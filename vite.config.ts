8537a8cc24a55eccbce8b91fe855d523fa2cc4b5
infohjewelry-cloud <info.hjewelry@gmail.com>
2025-11-27 13:05:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // importantissimo per Netlify
+  base: "./", // fondamentale per Netlify
 });
